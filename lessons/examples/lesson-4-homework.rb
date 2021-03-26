405e04b4376c3606b610e18953ad488c1c70fedb
Garllon <palluthe.bennet@gmail.com>
2021-03-26 15:42:46 +0100

    Create lesson-4-homework.rb

---
+++ lessons/examples/lesson-4-homework.rb
@@ -0,0 +1,43 @@
+## Basic soltion
+def calculcate_width(diagonal, ratio_width, ratio_height)
+  # Calculate squared hypotenuse
+  squared_ratio_diagonal = ratio_width**2 + ratio_height**2
+
+  # Calculate proportion
+  proportion = diagonal/Math.sqrt(squared_ratio_diagonal)
+
+  # transform ratio into unit of length
+  puts proportion * ratio_width
+end
+
+calculcate_width(32 ,16, 9)
+
+## Extra Part
+
+## with transform from inch to centimeter
+def calculcate_width(diagonal, ratio_width, ratio_height)
+  # Calculate squared hypotenuse
+  squared_ratio_diagonal = ratio_width**2 + ratio_height**2
+
+  # Calculate proportion
+  proportion = diagonal/Math.sqrt(squared_ratio_diagonal)
+
+  # transform ratio into unit of length
+  puts (proportion * ratio_width) * 2.54
+end
+
+calculcate_width(32 ,16, 9)
+
+# with a count of monitor
+def calculcate_width(diagonal, ratio_width, ratio_height, monitor_count)
+  # Calculate squared hypotenuse
+  squared_ratio_diagonal = ratio_width**2 + ratio_height**2
+
+  # Calculate proportion
+  proportion = diagonal/Math.sqrt(squared_ratio_diagonal)
+
+  # transform ratio into unit of length
+  puts ((proportion * ratio_width) * 2.54) * monitor_count
+end
+
+calculcate_width(32 ,16, 9, 2)
